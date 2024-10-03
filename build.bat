a4646eb779ba845522d27e592ed21dad19e6e199
Svend Ezaki Tofte <set@hmlab.dk>
2024-10-03 16:04:37 +0200

    Make build.bat take a debug param

--- build.bat
+++ build.bat
@@ -1,26 +1,31 @@
-@echo off
-mkdir out
-cl tree-sitter-c\src\parser.c /Foout\ts_c_parser.obj /c /MD /O2 /nologo
-cl tree-sitter-javascript\src\parser.c /Foout\ts_javascript_parser.obj /c /MD /O2 /nologo
-cl tree-sitter-javascript\src\scanner.c /Foout\ts_javascript_scanner.obj /c /MD /O2 /nologo
-set CLCMD=cl ^
+@ECHO OFF
+MKDIR out
+DEL /Q "out\*.*"
+DEL "test-editor\tslib.*"
+set BUILD_FLAGS=/O2
+IF "%~1"=="debug" (SET BUILD_FLAGS=/DEBUG /Z7)
+CL tree-sitter-c\src\parser.c /Foout\ts_c_parser.obj /c /MD %BUILD_FLAGS% /nologo
+CL tree-sitter-javascript\src\parser.c /Foout\ts_javascript_parser.obj /c /MD %BUILD_FLAGS% /nologo
+CL tree-sitter-javascript\src\scanner.c /Foout\ts_javascript_scanner.obj /c /MD %BUILD_FLAGS% /nologo
+set CLCMD=CL /nologo ^
 -I tree-sitter\highlight\include\ ^
 -I tree-sitter\lib\include ^
 -I tree-sitter-c\bindings\c\ ^
 -I tree-sitter-javascript\bindings\c\ ^
-/nologo /Foout\ ^
-/MD /O2 /DTREE_SITTER_HIDE_SYMBOLS ^
+/Foout\ ^
+/MD %BUILD_FLAGS% /DTREE_SITTER_HIDE_SYMBOLS ^
 tslib.c ^
-out\ts_c_parser.obj out\ts_javascript_parser.obj out\ts_javascript_scanner.obj ^
 tree-sitter\lib\src\lib.c ^
+out\ts_c_parser.obj out\ts_javascript_parser.obj out\ts_javascript_scanner.obj ^
 ws2_32.lib ntdll.lib advapi32.lib userenv.lib bcrypt.lib kernel32.lib user32.lib
 set CLCMDEXE=%CLCMD% main.c /Feout\main.exe
 set CLCMDLIB=%CLCMD% /LD /Feout\tslib.dll
 @echo on
 %CLCMDLIB%
 if %ErrorLevel%==0 (
-	copy out\tslib.dll test-editor\tslib.dll
-) 
+    copy out\tslib.dll test-editor
+    copy out\tslib.pdb test-editor
+)
 %CLCMDEXE%
 if %ErrorLevel%==0 (
 	out\main.exe
